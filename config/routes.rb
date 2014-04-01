5b10eb7f575e8f26b5c508f526b2393c48372540
Narin Kittikul <nkittikul313@gmail.com>
2014-04-01 16:12:13 -0700

    Added functionality to destroy topics, posts, and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Bloccit::Application.routes.draw do
   
-  get "comments/create"
-  get "tests/index"
-  get "tests/show"
-  get "tests/new"
-  get "tests/edit"
   devise_for :users
 
   resources :topics do
     resources :posts, except: [:index] do
-      resource :comments, only: [:create]
+      resources :comments, only: [:create, :destroy]
     end
   end
 
