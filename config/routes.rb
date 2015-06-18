ef046925e11e709631dd9aea49998197f7f6a9be
Narin Kittikul <nkittikul313@gmail.com>
2015-06-17 23:30:41 -0700

    fixed some bad routing practices and updated gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Bloccit::Application.routes.draw do
   resources :topics do
     resources :posts, except: [:index], controller: 'topics/posts' do
       resources :comments, only: [:create, :destroy]
-      get '/up-vote' => 'votes#up_vote', as: :up_vote
-      get '/down-vote' => 'votes#down_vote', as: :down_vote
+      post '/up-vote' => 'votes#up_vote', as: :up_vote
+      post '/down-vote' => 'votes#down_vote', as: :down_vote
       resources :favorites, only: [:create, :destroy]
     end
   end
@@ -15,8 +15,6 @@ Bloccit::Application.routes.draw do
   resources :users, only: [:show, :index, :update]
 
   ##match "about", to: 'welcome#about', via: :get
-
-  get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
 end
